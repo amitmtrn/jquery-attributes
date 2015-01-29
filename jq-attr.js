6aa0de8a17649cf937b64dadbab1da14da5bc38f
amitmtrn <amit@mtrn.co.il>
2015-01-30 00:52:54 +0200

    add first structure

---
+++ jq-attr.js
@@ -0,0 +1,28 @@
+/*global jQuery, console*/
+jQuery(function ($) {
+
+  'use strict';
+
+  var singleAction = ['toggle', 'show', 'hide', 'slideDown', 'slideUp', 'slideToggle', 'fadeIn', 'fadeOut', 'fateTo', 'fadeToggle'];
+  jQuery.each(singleAction, function (index, value) {
+    var $items = $('[data-jq-' + value + ']');
+
+    $items.each(function (itemIndex, item) {
+      var $item = $(item),
+        event = $item.attr('data-jq-event') || 'click';
+
+
+      $item.on(event, function (e) {
+        var $this = $(e.currentTarget),
+          selectorType = $item.attr('data-jq-selector-type') || 'siblings',
+          selector = $item.attr('data-jq-' + value);
+
+        $this[selectorType](selector)[value]();
+
+      });
+
+    });
+
+  });
+
+});
