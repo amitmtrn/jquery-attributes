3b9c8ef5dde603c503416f15b0b07477ba501db9
amitmtrn <amit@mtrn.co.il>
2015-02-16 15:45:55 +0200

    Disassemble the function

--- jq-attr.js
+++ jq-attr.js
@@ -4,21 +4,29 @@ jQuery( function( $ ) {
 
   var singleAction = [ "toggle", "show", "hide", "slideDown", "slideUp",
   "slideToggle", "fadeIn", "fadeOut", "fateTo", "fadeToggle" ];
-  jQuery.each( singleAction, function( index, value ) {
+
+  jQuery.each( singleAction, parseDataJq );
+//////////////////////////////////
+
+  function activateAction( e ) {
+    var $this = $( e.currentTarget ),
+    selectorType = $item.attr( "data-jq-selector-type" ) || "siblings",
+    selector = $item.attr( "data-jq-" + value );
+
+    $this[selectorType]( selector )[value]();
+
+  }
+
+  function setEventItem( itemIndex, item ) {
+    var $item = $( item ),
+    event = $item.attr( "data-jq-event" ) || "click";
+
+    $item.on( event, activateAction );
+  }
+
+  function parseDataJq( index, value ) {
     var $items = $( "[data-jq-" + value + "]" );
+    $items.each( setEventItem );
+  }
 
-    $items.each( function( itemIndex, item ) {
-      var $item = $( item ),
-      event = $item.attr( "data-jq-event" ) || "click";
-
-      $item.on( event, function( e ) {
-        var $this = $( e.currentTarget ),
-        selectorType = $item.attr( "data-jq-selector-type" ) || "siblings",
-        selector = $item.attr( "data-jq-" + value );
-
-        $this[selectorType]( selector )[value]();
-
-      } );
-    } );
-  } );
 } );
